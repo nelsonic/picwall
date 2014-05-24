6afa27b268cbf8711285f4c605c0ca1574a3085a
nelsonic <nodecoder@gmail.com>
2014-05-25 00:22:31 +0100

    node-static serves 404

--- static.js
+++ static.js
@@ -2,13 +2,21 @@ var static = require('node-static');
 var file = new static.Server('./static');
 require('http').createServer(function (request, response) {
 
-// Check if file is in accepted file types
+
+// Check if file is an accepted file type?
+
+
 // fs.exists('/etc/passwd', function (exists) {
 //   util.debug(exists ? "it's there" : "no passwd!");
+// if(exists){
+// 	file.serve(request, response);
+// } else {
+	file.serveFile('/404.html', 404, {}, request, response);
+// }
 // });
     //
     // Serve files!
     //
-    file.serve(request, response);
+
 
 }).listen(8080);
