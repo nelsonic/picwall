b22001d44213f5848a4594bb68119777dbd7d6f2
nelsonic <nodecoder@gmail.com>
2014-05-24 23:24:59 +0100

    [WiP] static check file exists before serving

--- static.js
+++ static.js
@@ -2,6 +2,10 @@ var static = require('node-static');
 var file = new static.Server('./static');
 require('http').createServer(function (request, response) {
 
+// Check if file is in accepted file types
+// fs.exists('/etc/passwd', function (exists) {
+//   util.debug(exists ? "it's there" : "no passwd!");
+// });
     //
     // Serve files!
     //
