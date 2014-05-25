1b3daef2a9e398499b0aab57347025d29d3c0dc1
nelsonic <nodecoder@gmail.com>
2014-05-25 23:59:33 +0100

    app working. try it!

--- static.js
+++ static.js
@@ -1,9 +1,8 @@
-var static    = require('node-static');
-    staticDir = './static',
-	file      = new static.Server(staticDir),
-	fs        = require('fs'),
-	path	  = require('path');
-
+var fs    = require('fs'),
+nstatic   = require('node-static'),
+staticDir = './static',
+file      = new nstatic.Server(staticDir),
+path      = require('path');
 
 require('http').createServer(function (req, res) {
 	var validFile = false;
@@ -12,23 +11,22 @@ require('http').createServer(function (req, res) {
 	// Check if file is an accepted file type?
 
 	console.log('R',req.url);
-	var f = __dirname+'/static'+req.url;
 
 	// only serve specific file types
 	if(req.url.indexOf('.') > -1){
-		ext = path.extname(req.ulr).toLowerCase();	
-		console.log('EXT:',ext);
+		var f = __dirname+'/static'+req.url;
+		var ext = path.extname(req.url).toLowerCase();
+		console.log('EXT >> ',ext);
+
+		fs.exists(f, function (exists) {
+			console.log(f,exists);
+			if(exists){
+				file.serve(req, res);
+			} else {
+				file.serveFile('/404.html', 404, {}, req, res);
+			}
+		});
 	}
-
-
-	fs.exists(f, function (exists) {
-		console.log(f,exists);
-		if(exists){
-			file.serve(req, res);
-		} else {
-			file.serveFile('/404.html', 404, {}, req, res);
-		}
-	});
     //
     // Serve files!
     //
